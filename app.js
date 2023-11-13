d7555bcf17538425fd8c2b1a83fda0f5736e3a42
Sanusi Muhyideen <just4deen@yahoo.com>
2023-11-13 22:38:47 +0100

    create a save button functionlity

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // variable decleration
 let incrementBtn = document.getElementById("increment-btn");
-
 let countEl = document.getElementById("count");
+let saveBtn = document.getElementById("save-btn");
+let saveEl = document.getElementById("save-el");
 
 // intialize the count as 0
 let count = 0;
@@ -9,7 +10,20 @@ let count = 0;
 // listen for clicks on the increment button
 incrementBtn.addEventListener("click", function () {
   // increment the count variable when the button is clicked
-  count = count + 1;
+  count += 1;
   // change the count-el in the HTML to reflect the new count
-  document.getElementById("count").innerHTML = count;
+  document.getElementById("count").textContent = count;
 });
+
+// Save button functionality
+// listen for clicks on the save button
+saveBtn.addEventListener("click", function () {
+  // Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let strCount = count + " - ";
+  //   Render the variable in the saveEl using innerText
+  saveEl.textContent += strCount;
+});
+
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+// 3. Render the variable in the saveEl using innerText
+// NB: Make sure to not delete the existing content of the paragraph
